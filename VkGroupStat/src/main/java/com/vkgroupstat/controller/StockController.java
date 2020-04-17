9b399863bbe92a036c2d041521197cf8661ec08a
Anton Saushkin <saushkin_anton@mail.ru>
2020-04-17 17:06:53 +0300

    Слил с dev, могу собрать и запустить в мавен.
    - Не получается настроить конфигурацию Spring Boot

--- VkGroupStat/src/main/java/com/vkgroupstat/controller/StockController.java
+++ VkGroupStat/src/main/java/com/vkgroupstat/controller/StockController.java
@@ -1,29 +1,82 @@
 package com.vkgroupstat.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.vkgroupstat.model.AjaxResponseBody;
 import com.vkgroupstat.model.Group;
+import com.vkgroupstat.model.SearchCriteria;
+import com.vkgroupstat.service.FeedbackService;
 import com.vkgroupstat.service.GroupService;
-import com.vkgroupstat.vkconnection.TEST.TEST_StringOut;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSender;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
+
 
 @RestController
+@RequestMapping("/api")
 public class StockController {
+	@Autowired
+	private MailSender mailSender;
+//
+//	@Value("checkins.tracker@gmail.com")
+//	String email="checkins.tracker@gmail.com";
+	private final GroupService service;
 
-	private final GroupService service;		
 	public StockController(GroupService service) {
 		this.service = service;
 	}
 
-	@RequestMapping("/findgroup")
-	public String returnSubscriptions(@RequestParam String groupName) {
-		Group group = service.groupRequestHandler(groupName);
-		return TEST_StringOut.groupToString(group);
+
+		LinkedHashMap<String,Integer> myLinkedHashMap =  new LinkedHashMap<String, Integer>();
+
+	@PostMapping("/findgroup")
+	public ResponseEntity<?> getSearchResultViaAjax( @RequestBody SearchCriteria search, Errors errors) {
+
+		AjaxResponseBody result = new AjaxResponseBody();
+
+		if (errors.hasErrors()) {
+
+			result.setMsg(errors.getAllErrors().stream().map(x -> x.getDefaultMessage()).collect(Collectors.joining(",")));
+			return ResponseEntity.badRequest().body(result);
+
+		}
+		Group group = service.groupRequestHandler(search.getGroupName());
+		result.setGroup(group);
+		return ResponseEntity.ok(group);
+
 	}
-	@RequestMapping("/")
-	public String returnSubscriptions() {
-		return "<h1>Hello from VkGroupStat<h1>";
+
+	@RequestMapping(value = "/sendEmail", method = RequestMethod.POST)
+	public	@ResponseBody	String sendEmail(@RequestParam("email") String emailUser,
+					 @RequestParam("subject") String subject,
+					 @RequestParam("message") String message) {
+
+		FeedbackService feedbackService = new FeedbackService(mailSender);
+
+		if (!feedbackService.validateEmail(emailUser)) {
+			return "Your email address is invalid. Please enter a valid address.";
+		}
+
+		if (message.trim().isEmpty()) {
+			return "Please enter the message.";
+		}
+
+		feedbackService.sendMsg(emailUser, subject, message);
+
+		return "The message has been sent!";
 	}
-	
+	//тестовые методы
+
+
 }
