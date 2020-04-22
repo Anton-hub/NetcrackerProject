3364a9a264fba3657f9a3382ffea92d1367a651a
Anton Saushkin <saushkin_anton@mail.ru>
2020-04-22 22:57:45 +0300

    Hot-Fix (GroupRepository)

--- src/main/java/com/vkgroupstat/repository/GroupRepository.java
+++ src/main/java/com/vkgroupstat/repository/GroupRepository.java
@@ -10,6 +10,5 @@ import java.util.List;
 @Repository
 public interface GroupRepository extends MongoRepository<Group, String>{
 	public Group findByurlName(String groupName);
-	public List<Group> findByUserId(Integer userId);
 
 }
